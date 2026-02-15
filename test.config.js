92c11bb4ed39e52a0d202a0196113d07b62ff205
Wituareard <87208350+Wituareard@users.noreply.github.com>
2026-02-15 20:15:01 +0100

    Update Config “config”

--- test.config.js
+++ test.config.js
@@ -1,6 +1,6 @@
 undefined
 
 export default {
-  foo: "uaaabar",
+  foo: "uaaabarf",
   _code: "undefined\n\nexport default {\n  foo: \"aaabar\",\n  code: \"undefined\\n\\nexport default {\\n  foo: \\\"abar\\\",\\n  code: \\\"import fs from \\\\\\\"node:fs\\\\\\\"\\\\n\\\\nexport default {\\\\n  foo: \\\\\\\"bar\\\\\\\"\\\\n}\\\\n\\\"\\n};\\n\",\n  _code: undefined\n};\n"
 };
